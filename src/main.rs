19ef2fd42b5d2adbf27789e3d3fcd3b29e83d6a3
Davide Usberti <usbertibox@gmail.com>
2021-08-28 19:50:15 +0200

    full commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    if cfg!(windows) {
+        std::process::Command::new("cls").status().unwrap();
+    } else if cfg!(unix) {
+        std::process::Command::new("clear").status().unwrap();
+    } else if cfg!(target_os = "macos") {
+        std::process::Command::new("clear").status().unwrap();
+    }
+}
